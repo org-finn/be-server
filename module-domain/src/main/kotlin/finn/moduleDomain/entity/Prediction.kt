a27ca8708bf8e1fb765acff2810a1f1e78b53288
anacnoda77 <sm1108shin@naver.com>
2025-08-18 16:00:37 +0900

    Feat: Prediction Score 계산 로직 추가

--- module-domain/src/main/kotlin/finn/moduleDomain/entity/Prediction.kt
+++ module-domain/src/main/kotlin/finn/moduleDomain/entity/Prediction.kt
@@ -7,23 +7,46 @@ class Prediction private constructor(
     val tickerId: UUID,
     val tickerCode: String,
     val shortCompanyName: String,
-    val predictionStrategy: PredictionStrategy,
-    val sentiment: Int,
-    val newsCount: Int,
+    val positiveNewsCount: Int,
+    val negativeNewsCount: Int,
+    val neutralNewsCount: Int,
     val sentimentScore: Int,
+    val predictionStrategy: PredictionStrategy,
     val predictionDate: LocalDateTime
 ) {
 
     companion object {
         fun create(
             tickerId: UUID, tickerCode: String, shortCompanyName: String,
-            predictionStrategy: PredictionStrategy, sentiment: Int,
-            newsCount: Int, sentimentScore: Int, predictionDate: LocalDateTime
+            positiveNewsCount: Int, negativeNewsCount: Int,
+            neutralNewsCount: Int, sentimentScore: Int,
+            predictionDate: LocalDateTime
         ): Prediction {
             return Prediction(
-                tickerId, tickerCode, shortCompanyName, predictionStrategy,
-                sentiment, newsCount, sentimentScore, predictionDate
+                tickerId,
+                tickerCode,
+                shortCompanyName,
+                positiveNewsCount,
+                negativeNewsCount,
+                neutralNewsCount,
+                sentimentScore,
+                getStrategyFromScore(sentimentScore),
+                predictionDate
             )
+        }
+
+        fun getStrategyFromScore(sentimentScore: Int): PredictionStrategy {
+            return PredictionStrategy.entries.firstOrNull { strategy ->
+                sentimentScore > strategy.left && sentimentScore <= strategy.right
+            } ?: throw IllegalArgumentException("유효하지 않은 sentimentScore: $sentimentScore")
+        }
+    }
+
+    fun getNewsCountAlongWithStrategy(): Int {
+        return when (predictionStrategy) {
+            PredictionStrategy.STRONG_BUY, PredictionStrategy.WEEK_BUY -> positiveNewsCount
+            PredictionStrategy.NEUTRAL -> neutralNewsCount
+            PredictionStrategy.STRONG_SELL, PredictionStrategy.WEEK_SELL -> negativeNewsCount
         }
     }
 
--- module-domain/src/main/kotlin/finn/moduleDomain/entity/PredictionStrategy.kt
+++ module-domain/src/main/kotlin/finn/moduleDomain/entity/PredictionStrategy.kt
@@ -1,9 +1,9 @@
 package finn.moduleDomain.entity
 
-enum class PredictionStrategy(val strategy: String) {
-    STRONG_BUY("강한 매수"),
-    WEEK_BUY("약한 매수"),
-    NEUTRAL("중립"),
-    WEEK_SELL("약한 매도"),
-    STRONG_SELL("강한 매도")
+enum class PredictionStrategy(val strategy: String, val left: Int, val right: Int) {
+    STRONG_BUY("강한 매수", 80, 100),
+    WEEK_BUY("약한 매수", 60, 80),
+    NEUTRAL("중립", 40, 60),
+    WEEK_SELL("약한 매도", 20, 40),
+    STRONG_SELL("강한 매도", 0, 20)
 }
