2bffba3b0076837dd26accc42d52a73dccdd77d4
anacnoda77 <sm1108shin@naver.com>
2025-08-28 18:15:31 +0900

    Fix: origin 제대로 주입되지 않는 문제 해결

--- module-api/src/main/kotlin/finn/config/WebConfig.kt
+++ module-api/src/main/kotlin/finn/config/WebConfig.kt
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    @Value("\${cors.allowed-origins}") private val allowedOrigins: String
+    @Value("\${cors.allowed-origins}")
+    private val allowedOrigins: String
 ) : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**") // /api/ 경로 하위의 모든 엔드포인트에 적용
-            .allowedOrigins(*allowedOrigins.split(",").toTypedArray()) // 허용할 도메인
+            .allowedOriginPatterns(*allowedOrigins.split(",").toTypedArray()) // 허용할 도메인
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 허용할 헤더
             .allowCredentials(true) // 쿠키 등 자격 증명 허용
