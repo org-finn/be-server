c2296deb3de14cb3f47757cf588874319362a0eb
anacnoda77 <sm1108shin@naver.com>
2025-09-02 17:13:50 +0900

    Remove: S3 관련 활성화 부분 제거

--- module-api/src/main/kotlin/finn/orchestrator/TickerPriceOrchestrator.kt
+++ module-api/src/main/kotlin/finn/orchestrator/TickerPriceOrchestrator.kt
@@ -4,20 +4,15 @@ import finn.mapper.toDto
 import finn.response.graph.TickerGraphResponse
 import finn.response.graph.TickerRealTimeGraphListResponse
 import finn.response.graph.TickerRealTimeGraphListResponse.TickerRealTimeGraphResponse
-import finn.s3.S3Service
 import finn.service.GraphQueryService
 import finn.transaction.ExposedTransactional
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 @ExposedTransactional(readOnly = true)
 class TickerPriceOrchestrator(
-    private val graphQueryService: GraphQueryService,
-    private val s3Service: S3Service,
-    @Value("\${cloud.aws.s3.bucket}")
-    private val bucketName: String
+    private val graphQueryService: GraphQueryService
 ) {
     fun getTickerGraphData(tickerId: UUID, period: String): TickerGraphResponse {
         val graphData = graphQueryService.getTickerGraphData(tickerId, period)
