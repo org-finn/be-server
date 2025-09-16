879bc9580494116d684e06ae13099236c71a364c
anacnoda77 <sm1108shin@naver.com>
2025-09-16 22:27:10 +0900

    Feat: 캐싱 AOP 분리

--- module-api/src/main/kotlin/finn/service/TickerQueryService.kt
+++ module-api/src/main/kotlin/finn/service/TickerQueryService.kt
@@ -10,11 +10,15 @@ class TickerQueryService(
     private val tickerRepository: TickerRepository
 ) {
 
-    fun getTickerSearchList(keyword: String) : List<TickerSearchQueryDto> {
-        return tickerRepository.getTickerListBySearchKeyword(keyword)
+    fun getTickerSearchList(keyword: String): List<TickerSearchQueryDto> {
+        val tickerList = tickerRepository.findAll()
+        return tickerList.filter {
+            it.shortCompanyName().startsWith(keyword, ignoreCase = true) ||
+                    it.shortCompanyNameKr().startsWith(keyword, ignoreCase = true)
+        }
     }
 
-    fun getTickerByTickerCode(tickerCode: String) : Ticker {
+    fun getTickerByTickerCode(tickerCode: String): Ticker {
         return tickerRepository.getTickerByTickerCode(tickerCode)
     }
 }
--- module-persistence/src/main/kotlin/finn/repository/impl/TickerRepositoryImpl.kt
+++ module-persistence/src/main/kotlin/finn/repository/impl/TickerRepositoryImpl.kt
@@ -1,46 +1,28 @@
 package finn.repository.impl
 
-import finn.cache.CacheUtil
 import finn.entity.query.Ticker
 import finn.mapper.toDomain
 import finn.queryDto.TickerSearchQueryDto
 import finn.repository.TickerRepository
 import finn.repository.exposed.TickerExposedRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Repository
 
 @Repository
 class TickerRepositoryImpl(
     private val tickerExposedRepository: TickerExposedRepository,
-    private val cacheUtil: CacheUtil
 ) : TickerRepository {
 
     companion object {
-        private const val TICKER_LIST_CACHE_KEY = "tickers:all"
-    }
-
-
-    override fun getTickerListBySearchKeyword(keyword: String): List<TickerSearchQueryDto> {
-        val tickerList = findAll()
-        return tickerList.filter {
-            it.shortCompanyName().startsWith(keyword, ignoreCase = true) ||
-                    it.shortCompanyNameKr().startsWith(keyword, ignoreCase = true)
-        }
+        private const val TICKER_LIST_CACHE_KEY = "'tickers:all'"
     }
 
     override fun getTickerByTickerCode(tickerCode: String): Ticker {
         return toDomain(tickerExposedRepository.findByTickerCode(tickerCode))
     }
 
-    private fun findAll(): List<TickerSearchQueryDto> {
-        val cachedData = cacheUtil.get(TICKER_LIST_CACHE_KEY, List::class.java)
-
-        if (cachedData != null) {
-            @Suppress("UNCHECKED_CAST")
-            return cachedData as List<TickerSearchQueryDto>
-        }
-
-        val tickersFromDb = tickerExposedRepository.findAll()
-        cacheUtil.set(TICKER_LIST_CACHE_KEY, tickersFromDb, 86400L)
-        return tickersFromDb
+    @Cacheable("tickerSearchList", key = TICKER_LIST_CACHE_KEY)
+    override fun findAll(): List<TickerSearchQueryDto> {
+        return tickerExposedRepository.findAll()
     }
 }
