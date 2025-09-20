5a6ffaee2618f5a915a24c9faa290cd084b7fec3
anacnoda77 <sm1108shin@naver.com>
2025-09-20 17:38:15 +0900

    Feat: PageRequest에 새로운 필터링 조건 적용

--- module-api/src/main/kotlin/finn/paging/ArticlePageRequest.kt
+++ module-api/src/main/kotlin/finn/paging/ArticlePageRequest.kt
@@ -1,23 +1,28 @@
 package finn.paging
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.util.*
 
-data class ArticlePageRequest (
+data class ArticlePageRequest(
     @field:Schema(description = "페이지 번호", defaultValue = "0")
     override val page: Int = 0,
     @field:Schema(description = "페이지 당 데이터 개수", defaultValue = "10")
     override val size: Int = 10,
     @field:Schema(
-        description = "필터링 기준",
+        description = "종목 필터링 기준",
+        defaultValue = ""
+    )
+    val tickerId: UUID,
+    @field:Schema(
+        description = "감정 필터링 기준",
         defaultValue = "all",
-//        allowableValues = ["all", "positive", "negative"]
-        allowableValues = ["all"]
-        )
-    val filter: String,
+        allowableValues = ["all", "positive", "negative"]
+    )
+    val sentiment: String,
     @field:Schema(
         description = "정렬 기준",
         defaultValue = "recent",
         allowableValues = ["recent"]
     )
     val sort: String
-): PageRequest
+) : PageRequest
