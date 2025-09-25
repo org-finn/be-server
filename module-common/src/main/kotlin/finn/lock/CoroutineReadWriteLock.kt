2be5c2b0ab377187d0432602619801209e50619d
anacnoda77 <sm1108shin@naver.com>
2025-09-25 15:59:05 +0900

    Fix: 예외가 발생해도 안전하게 락을 반납하도록 lock 로직 변경

--- module-common/src/main/kotlin/finn/lock/CoroutineReadWriteLock.kt
+++ module-common/src/main/kotlin/finn/lock/CoroutineReadWriteLock.kt
@@ -19,10 +19,13 @@ class CoroutineReadWriteLock(maxReaders: Int = Int.MAX_VALUE) {
      */
     suspend fun <T> read(action: suspend () -> T): T {
         // 쓰기 락을 획득할 수 있어야 읽기 락 획득 가능
-        writeLock.lock()
-        readLock.acquire()
-        // 읽기 락을 획득하였으므로 쓰기 락 즉시 해제
-        writeLock.unlock()
+        try {
+            writeLock.lock()
+            readLock.acquire()
+        } finally {
+            // 읽기 락을 획득하였으므로 쓰기 락 즉시 해제
+            writeLock.unlock()
+        }
 
         try {
             return action()
@@ -32,12 +35,11 @@ class CoroutineReadWriteLock(maxReaders: Int = Int.MAX_VALUE) {
     }
 
     suspend fun <T> write(action: suspend () -> T): T {
-        writeLock.lock()
-        // 현재 진행 중인 모든 읽기 작업이 끝날 때까지 대기
-        // 모든 reader의 permit을 획득
-        repeat(readLock.availablePermits) { readLock.acquire() }
-
         try {
+            writeLock.lock()
+            // 현재 진행 중인 모든 읽기 작업이 끝날 때까지 대기
+            // 모든 reader의 permit을 획득
+            repeat(readLock.availablePermits) { readLock.acquire() }
             return action()
         } finally {
             // 모든 reader의 permit을 다시 반납
