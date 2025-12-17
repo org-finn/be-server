2f44e23780454bed87f0e6f30dddb8ea4180e4a6
anacnoda77 <sm1108shin@naver.com>
2025-12-17 14:36:18 +0900

    Feat: article_summary 반드시 최신 데이터를 리턴하도록 하여 데이터가 아예 전달되지 않는 상황 방지(추후 로깅 로직 삽입 필요)

--- module-persistence/src/main/kotlin/finn/repository/exposed/ArticleSummaryExposedRepository.kt
+++ module-persistence/src/main/kotlin/finn/repository/exposed/ArticleSummaryExposedRepository.kt
@@ -6,39 +6,29 @@ import finn.exception.CriticalDataOmittedException
 import finn.table.ArticleSummaryAllTable
 import finn.table.ArticleSummaryTable
 import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.javatime.date
 import org.springframework.stereotype.Repository
-import java.time.Clock
-import java.time.LocalDate
 import java.util.*
 
 @Repository
-class ArticleSummaryExposedRepository(
-    private val clock: Clock
-) {
+class ArticleSummaryExposedRepository{
 
     fun findSummaryAll(): ArticleSummaryAllExposed {
-        return ArticleSummaryAllExposed
-            .find {
-                ArticleSummaryAllTable.summaryDate.date() greaterEq LocalDate.now(clock)
-            }
+        return ArticleSummaryAllExposed.all()
             .orderBy(ArticleSummaryAllTable.summaryDate to SortOrder.DESC)
             .limit(1)
             .firstOrNull()
-            ?: throw CriticalDataOmittedException("금일 날짜로 생성된 종합 뉴스 데이터를 찾을 수 없습니다.")
+            ?: throw CriticalDataOmittedException("종합 뉴스 요약 데이터를 찾을 수 없습니다.")
     }
 
     fun findByTickerId(tickerId: UUID): ArticleSummaryExposed {
         return ArticleSummaryExposed
             .find {
-                (ArticleSummaryTable.tickerId eq tickerId) and
-                        (ArticleSummaryTable.summaryDate.date() greaterEq LocalDate.now(clock))
+                (ArticleSummaryTable.tickerId eq tickerId)
             }
             .orderBy(ArticleSummaryTable.summaryDate to SortOrder.DESC)
             .limit(1)
             .firstOrNull()
-            ?: throw CriticalDataOmittedException("금일 날짜로 생성된 $tickerId 뉴스 데이터를 찾을 수 없습니다.")
+            ?: throw CriticalDataOmittedException("$tickerId 뉴스 요약 데이터를 찾을 수 없습니다.")
 
     }
 }
