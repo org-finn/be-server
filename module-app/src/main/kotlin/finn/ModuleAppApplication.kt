6199d53fca0de1c908e8cbf7314b7f76becd1247
anacnoda77 <sm1108shin@naver.com>
2026-02-03 14:51:41 +0900

    Chore: scheduling 허용 애노테이션 명시

--- module-app/src/main/kotlin/finn/ModuleAppApplication.kt
+++ module-app/src/main/kotlin/finn/ModuleAppApplication.kt
@@ -5,9 +5,11 @@ import finn.config.kis.KisProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableConfigurationProperties(GoogleOAuthConfig::class, KisProperties::class)
+@EnableScheduling
 class ModuleAppApplication
 
 fun main(args: Array<String>) {
