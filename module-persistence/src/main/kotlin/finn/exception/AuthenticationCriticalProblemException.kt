7501fb7985c7dc3c43ade20a4d1453ae0493ea27
anacnoda77 <sm1108shin@naver.com>
2026-02-10 12:43:42 +0900

    Refactor: 인증 db 에러 예외 상태코드 변경

--- module-persistence/src/main/kotlin/finn/exception/AuthenticationCriticalProblemException.kt
+++ module-persistence/src/main/kotlin/finn/exception/AuthenticationCriticalProblemException.kt
@@ -2,7 +2,7 @@ package finn.exception
 
 // 인증 관련 DB 로직 문제 발생
 class AuthenticationCriticalProblemException(message: String, cause: Throwable?) :
-    ServerErrorException(message, cause) {
+    UnAuthorizedException(message, cause) {
 
     constructor(message: String) : this(message, null)
 }
